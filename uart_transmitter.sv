2a9c1b7bc47d26ee472663686afebf1ea8bc5ee7
LIJIN WILSON <112461401+LijinWilson@users.noreply.github.com>
2026-06-07 17:04:14 +0530

    Enhance uart_transmitter.sv with comments for clarity
    Added comments to clarify parameter purposes and logic signals.

--- uart_transmitter.sv
+++ uart_transmitter.sv
@@ -1,13 +1,13 @@
 module Uart_Transmitter (uart_if uart);
 
-    parameter int clk_freq = 50_000_000;
-    parameter int baud_rate = 115200;
-    parameter int div_counter = clk_freq / baud_rate;
+    parameter int clk_freq = 50_000_000;    // 50 million cycle per second
+    parameter int baud_rate = 115200;    // bits per second
+    parameter int div_counter = clk_freq / baud_rate;     // 434 cycle is used to send 1 bit.
 
-    logic [8:0] cycle_counter;
-    logic [3:0] bit_index;
-    logic [9:0] tx_shift;
-    logic sending;
+    logic [8:0] cycle_counter;    // count from 0 to 433, this time to wait for before sending next bit.
+    logic [3:0] bit_index;   // Track which bit is being out of 8
+    logic [9:0] tx_shift;    // Store the full data, including the {start, data, stop}.
+    logic sending;    // Flag used to data is tranmitting or not.
 
     always_ff @(posedge uart.clk or posedge uart.reset) begin
         if (uart.reset) begin
@@ -17,13 +17,13 @@ module Uart_Transmitter (uart_if uart);
             bit_index <= 0;
             uart.busy <= 0;
         end else begin
-            if (uart.transmit && !sending) begin
-                tx_shift <= {1'b1, uart.TxData, 1'b0};
+            if (uart.transmit && !sending) begin    // user request data transmission and currently no data is in sending status
+                tx_shift <= {1'b1, uart.TxData, 1'b0};    // {start_bit, Data, Stop_bit}
                 sending <= 1;
                 bit_index <= 0;
                 cycle_counter <= 0;
-                uart.busy <= 1;
-            end else if (sending) begin
+                uart.busy <= 1;    // indicates UART is bussy in sending data 
+            end else if (sending) begin    // Data is in tranmission progress
                 if (cycle_counter == div_counter - 1) begin
                     cycle_counter <= 0;
                     uart.Txd <= tx_shift[bit_index];
